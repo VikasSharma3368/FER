4cfdf4fe65288cb88e41ac272b4f391c791dae4d
VikasSharma3368 <96167843+VikasSharma3368@users.noreply.github.com>
2022-05-18 15:56:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,11 +16,7 @@ classifier =load_model(r'F:\Vikas\realtimefaceemotionrecognition\model.h5')
 
 emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
 
-#load face
-try:
-    face_cascade = cv2.CascadeClassifier(r"F:\Vikas\realtimefaceemotionrecognition\haarcascade_frontalface_default.xml")
-except Exception:
-    st.write("Error loading cascade classifiers")
+
 
 
 cap = cv2.VideoCapture(0)
@@ -59,31 +55,7 @@ cv2.destroyAllWindows()
 
 
 
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
 
-        #image gray
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(
-            image=img_gray, scaleFactor=1.3, minNeighbors=5)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img=img, pt1=(x, y), pt2=(
-                x + w, y + h), color=(255, 0, 0), thickness=2)
-            roi_gray = img_gray[y:y + h, x:x + w]
-            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-            if np.sum([roi_gray]) != 0:
-                roi = roi_gray.astype('float') / 255.0
-                roi = img_to_array(roi)
-                roi = np.expand_dims(roi, axis=0)
-                prediction = classifier.predict(roi)[0]
-                maxindex = int(np.argmax(prediction))
-                finalout = emotion_labels[maxindex]
-                output = str(finalout)
-            label_position = (x, y)
-            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-        return img
 
 def main():
     # Face Analysis Application #
